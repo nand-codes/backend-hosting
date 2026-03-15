a7fe1f5d2a19f07a0840fff48420ac4017a73ed0
nand-codes <nandakishornandu4@gmail.com>
2026-03-15 21:39:33 +0530

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 from api.chat import router as chat_router
 from api.upload import router as upload_router
@@ -21,3 +23,8 @@ app.add_middleware(
 # register routes
 app.include_router(chat_router)
 app.include_router(upload_router)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
